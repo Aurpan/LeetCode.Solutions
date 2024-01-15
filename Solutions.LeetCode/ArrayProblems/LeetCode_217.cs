13c073066b2a8af6358c914b4d932529fc28a2c2
Aurpan Dash <33072535+Aurpan@users.noreply.github.com>
2024-01-16 03:19:51 +0600

    Feat: Add solution for Prob 217

---
+++ Solutions.LeetCode/ArrayProblems/LeetCode_217.cs
@@ -0,0 +1,42 @@
+﻿namespace Solutions.LeetCode.ArrayProblems
+{
+    // 217. Contains Duplicate
+    public class LeetCode_217
+    {
+        // Brute Force Solution
+        // T.C => O(n logn) + O(n)
+        // S.C => O(n)
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Array.Sort(nums); // T.C => O(n logn)
+
+            for (int i = 0; i < (nums.Length - 1); i++) // T.C => O(n)
+            {
+                if (nums[i] == nums[i + 1])
+                    return true;
+            }
+
+            return false;
+        }
+
+
+
+        // Btter Solution
+        // T.C => 2 * O(n) => O(n)
+        // S.C => O(n)
+        public bool ContainsDuplicate_Better(int[] nums)
+        {
+            Dictionary<int, bool> numsDict = new Dictionary<int, bool>(); // S.C => O(n)
+
+            for (int i = 0; i < nums.Length; i++) // T.C => O(n)
+            {
+                if (numsDict.TryGetValue(nums[i], out bool val)) // T.C => O(1)
+                    return true;
+
+                numsDict.Add(nums[i], false); // T.C => O(1)
+            }
+
+            return false;
+        }
+    }
+}
--- Solutions.LeetCode/Program.cs
+++ Solutions.LeetCode/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Solutions.LeetCode
+﻿using Solutions.LeetCode.ArrayProblems;
+
+namespace Solutions.LeetCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region 217. Contains Duplicate
+
+            var obj217 = new LeetCode_217();
+            int[] numsArray = { 1, 2, 3, 1};
+
+            Console.WriteLine(obj217.ContainsDuplicate(numsArray));
+            Console.WriteLine(obj217.ContainsDuplicate_Better(numsArray));
+
+            #endregion
         }
     }
 }
