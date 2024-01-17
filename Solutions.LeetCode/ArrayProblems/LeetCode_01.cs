5b5cb137dbfa13998f30810dd79b2394d37bc8b5
Aurpan Dash <33072535+Aurpan@users.noreply.github.com>
2024-01-18 02:30:39 +0600

    Feat: Add solution for LeetCode prob 1

---
+++ Solutions.LeetCode/ArrayProblems/LeetCode_01.cs
@@ -0,0 +1,47 @@
+﻿namespace Solutions.LeetCode.ArrayProblems
+{
+    // 1. Two Sum
+    public class LeetCode_01
+    {
+        // Brute Force Solution
+        // T.C. => O(n^2)
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++) // T.C. => O(n)
+            {
+                for (int j = i + 1; j < nums.Length; j++) // T.C. => O(n)
+                {
+                    if (nums[i] + nums[j] == target)
+                        return [i, j];
+                }
+            }
+
+            return new int[2];
+        }
+
+
+
+        // Better Solution
+        // T.C. => O(2 * n) => O(n)
+        // S.C. => O(n)
+        public int[] TwoSum_Better(int[] nums, int target)
+        {
+            if (nums.Length == 2)
+                return [0, 1];
+
+            Dictionary<int, int> numsDict = new Dictionary<int, int>(); // S.C. => O(n)
+
+            for (int i = 0; i < nums.Length; i++) // T.C. => O(n)
+            {
+                if (numsDict.ContainsKey(target - nums[i])) // O(1)
+                    return [numsDict[target - nums[i]], i];
+
+
+                if (!numsDict.ContainsKey(nums[i])) // O(1)
+                    numsDict.Add(nums[i], i);
+            }
+
+            return new int[2];
+        }
+    }
+}
--- Solutions.LeetCode/Program.cs
+++ Solutions.LeetCode/Program.cs
@@ -20,12 +20,29 @@ namespace Solutions.LeetCode
 
             #region 242. Valid Anagram
 
-            var obj242 = new StringProblems.LeetCode_242();
-            var obj242_arr = new ArrayProblems.LeetCode_242();
+            //var obj242 = new StringProblems.LeetCode_242();
+            //var obj242_arr = new ArrayProblems.LeetCode_242();
 
-            Console.WriteLine(obj242.IsAnagram("car", "cat"));
+            //Console.WriteLine(obj242.IsAnagram("car", "cat"));
 
             #endregion
+
+
+            #region 1. Two Sum
+
+            var obj01 = new ArrayProblems.LeetCode_01();
+
+            int[] nums = { 2, 5, 5, 11 };
+            //var result = obj01.TwoSum(nums, 10);
+            var result = obj01.TwoSum_Better(nums, 10);
+            
+            foreach (int num in result) 
+            {
+                Console.WriteLine(num);
+            }
+
+            #endregion
+
 
 
         }
